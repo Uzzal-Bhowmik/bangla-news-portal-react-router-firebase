9daa1a430e2f19fc9f3e557c049cb246db0a25a3
Uzzal Bhowmik <ubcreativity111@gmail.com>
2023-06-30 15:32:32 +0600

    added news api data

--- bangla-news-server/index.js
+++ bangla-news-server/index.js
@@ -7,13 +7,20 @@ app.use(cors());
 
 //categories data
 const categories = require("./categories.json");
+// news data
+const news = require("./news.json");
 
+// get requests
 app.get("/", (req, res) => {
     res.send("Bangla News Server API")
 })
 
 app.get("/news-categories", (req, res) => {
     res.send(categories);
+})
+
+app.get("/all-news", (req, res) => {
+    res.send(news);
 })
 
 app.listen(port, () => {
